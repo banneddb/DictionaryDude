b27bb938963b6c30d97d3371b255219d52db9cfe
Behruz Omon <behruzomon6@gmail.com>
2024-12-20 20:17:36 -0600

    commit

--- startup.py
+++ startup.py
@@ -1,10 +1,18 @@
+import discord
 from typing import Final
 import os
+from discord.ext import commands
 from dotenv import load_dotenv
-from discord import Intents, Client, Message
 
-# Loading the token into the program
 load_dotenv()
-TOKEN: Final[str] = os.getenv('DISCORD_TOKEN')
+TOKEN = os.getenv('DISC_TOKEN')
+intents = discord.Intents.default()
+intents.message_content = True  
+bot = commands.Bot(command_prefix="?", intents=intents)
 
-client.run(TOKEN)
+print(TOKEN)
+@bot.event
+async def on_ready():
+    print('Successfully logged in and booted')
+
+bot.run(TOKEN)
