a38d7e818910c045d007cf4b1ee15f64d9be61f3
Behruz Omon <behruzomon6@gmail.com>
2024-12-27 12:56:47 -0600

    commit

--- cmds/general.py
+++ cmds/general.py
@@ -1,7 +1,12 @@
 import discord
 from typing import Final
 from discord.ext import commands
+import requests
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
+DICT_TOKEN = os.getenv('DICT_TOKEN')
 intents = discord.Intents.default()
 intents.message_content = True  #Can read messages
 bot = commands.Bot(command_prefix="?", intents=intents)
@@ -11,10 +16,26 @@ class GenCommands(commands.Cog):
         self.bot = bot
 
     @commands.command()
-    async def hello(self, ctx):
+    async def hello(self, ctx, ):
         await ctx.send("Hello!")
+
+    @commands.command()
+    async def define(self, ctx, *, word: str):
+        url = f"https://www.dictionaryapi.com/api/v3/references/collegiate/json/{word}?key={DICT_TOKEN}"
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            if data:
+                definition = data[0].get('shortdef')
+            else:
+                await ctx.send(f"No definition of {word} was found. Try again.")
+        else:
+            await ctx.send("There was an error processing your request. Try again.")
+
+
+        
 
 
 async def setup(bot):
-    await bot.add_cog(GenCommands(bot))
-    print("General commands succesfuly loaded")
+   await bot.add_cog(GenCommands(bot))
+   print("General commands succesfuly loaded")
